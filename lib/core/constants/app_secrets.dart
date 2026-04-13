441b162245d1a9e7e1857c155348637cd7e5e8c8
dev-ajijul-islam <devajijulislam@gmail.com>
2026-04-13 12:12:20 +0600

    app_secret file created

---
+++ lib/core/constants/app_secrets.dart
@@ -0,0 +1,11 @@
+class AppSecrets {
+  AppSecrets._();
+
+  // Stripe Keys
+  static const String stripePublishableKey = 'your_stripe_publishable_key_here';
+  static const String stripeSecretKey = 'your_stripe_secret_key_here';
+
+  // Other API Keys
+  static const String googleMapsApiKey = 'your_google_maps_api_key_here';
+  static const String firebaseApiKey = 'your_firebase_api_key_here';
+}
